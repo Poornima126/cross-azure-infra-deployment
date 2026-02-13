dc6b82bbcb9f3d0094a40533616672acafdf0d52
Poornima126 <pumapoornima777@gmail.com>
2026-02-13 16:15:16 +0530

    updated code

--- viziapps-poc-infra/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "Dev"
-    storage_account_name = "test1302"
-    container_name       = "tfstate"
-    key                  = "dev.terraform.tfstate"
-  }
-}
